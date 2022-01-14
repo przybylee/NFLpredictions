6870d1239eece031d5bc5fda8a8810a3fe60df1b
Lee Przybylski <przybylskily@gmail.com>
2022-01-14 16:08:13 -0600

    Added XY_differences

---
+++ R/XY_differences.R
@@ -0,0 +1,33 @@
+#' From a collection of game scores, create a design matrix X and a vector of differences Y_diff
+#' to use for linear regression.
+#'
+#' @param games
+#'
+#' @return a list
+#' @export
+#'
+#' @examples
+#' result <- XY_differences(sample)
+XY_differences <- function(games){
+  N <- length(games$Score)
+  M <- length(unique(games$Team))
+  X <- matrix(data = 0, nrow = N/2, ncol = M+1)
+  teams <- sort(unique(games$Team))
+  X <- matrix(data = 0, nrow = N/2, ncol = M+1)
+  colnames(X) <- c("home", teams)
+  Y <- rep(0, N/2)
+  #Fill in cols of X with 1's where appropriate
+  row = 1
+  for(n in seq(2,N, by = 2)){
+    X[row,"home"] <- 1
+    X[row, games$Team[n-1]] <- -1
+    X[row, games$Team[n]] <- 1
+    Y[row] <- games$Score[n] - games$Score[n-1]
+    row <- row +1
+  }
+  start <- paste(games$Year[1], ", week ", games$Week[1])
+  end <- paste(games$Year[N], ", week ", games$Week[N])
+  result <- list(X,Y, teams, games, start, end)
+  names(result) <- c("X", "Y_diff", "teams", "games", "start", "end")
+  return(result)
+}
