d01952bbe161b50039faa75c0d6fe289c26764ef
Lee Przybylski <przybylskily@gmail.com>
2022-01-15 12:30:24 -0600

    Updated documentation for point_spread_ols

--- R/point_spread_ols.R
+++ R/point_spread_ols.R
@@ -1,17 +1,28 @@
-AwayTm = "Kansas City Chiefs"
-HomeTm = "Tampa Bay Buccaneers"
-#Enter the moneyline
-line_away <- -163
-line_home <- 143
-spread <- -3
-#colnames(X)
-c <- c(1,rep(0, length(teams)))
-c[colnames(X) == HomeTm] <- 1
-c[colnames(X) == AwayTm] <- -1
-#Put contrast vector c into confint to estimate HomeTm score - AwayTm score
-conf_int(X, y = Y, cont = c, d = 0, a = 0.05)
-#Using the estimated difference in team effects, compute a win probability
-mu <- conf_int(X, y = Y, c = c, d = 0, a = 0.05)[1,1]
-sigma <- sqrt(sigsq <- anova(lm(Y ~ X+0))[2,3])
-AwayProb <- pnorm(0,mean = mu, sd = sigma)
-HomeProb <- pnorm(0,mean = mu, sd = sigma, lower.tail = FALSE)
+#' Estimate the point spread from a season design object
+#'
+#' @param data A list containing a design matrix, score differences, and teams,
+#' as produced by the XY_differences function
+#' @param home The name of the home team, may be a substring
+#' @param away The name of the away team, may be a substring
+#' @param home_effect Logical, TRUE if we want to estimate with home field advantage
+#' @param a The level of the confidence interval
+#'
+#' @return A data frame describing the estimated point spread and the limits of a
+#' confidence level
+#' @export
+#'
+#' @examples
+point_spread_ols <- function(data, home, away, home_effect = TRUE, a = 0.05){
+  teams <- data$teams
+  X <- data$X
+  Y <- data$Y_diff
+  home_indx <- which(grepl(home, teams, ignore.case = TRUE))[1]
+  away_indx <- which(grepl(away, teams, ignore.case = TRUE))[1]
+  HomeTm <- teams[home_indx]
+  AwayTm <- teams[away_indx]
+  cont <- c(ifelse(home_effect, 1, 0), rep(0, length(teams)))
+  cont[colnames(X) == HomeTm] <- 1
+  cont[colnames(X) == AwayTm] <- -1
+  #Put contrast vector c into confint to estimate HomeTm score - AwayTm score
+  conf_int_ols(X, y = Y, cont = cont, d = 0, a = a)
+}
