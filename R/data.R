81ae29693e08b163d82c89a22d628ecc758a5ab7
Lee Przybylski <przybylskily@gmail.com>
2022-01-16 14:02:10 -0600

    Added regssn2021 data

---
+++ R/data.R
@@ -0,0 +1,13 @@
+#' Results of games from the 2021 regular season
+#'
+#' A dataset containing the results of games from the 2021 NFL season
+#'
+#' @format A data frame with 544 rows and 4 variables:
+#' \describe{
+#'   \item{Team}{Team playing in the game.  Alternates away, home, away,...}
+#'   \item{Score}{Number of points scored}
+#'   \item{Year}{The year the game was played}
+#'   \item{Week}{The week in the season when the game was played}
+#' }
+#' @source \url{https://www.pro-football-reference.com/}
+"regssn2021"
