9938f330f621e7deb3e90e40f214b2d1f4771eeb
Lee Przybylski <przybylskily@gmail.com>
2022-02-21 18:28:58 -0600

    Added get_design function to replace XY_differences

--- R/XY_differences.R
+++ R/XY_differences.R
@@ -1,7 +1,7 @@
 #' From a collection of game scores, create a design matrix X and a vector of differences Y_diff
 #' to use for linear regression.
 #'
-#' @param games A data frame with columns for Team, Score, Year, and Week produced by scrape_games.  Last 3 columns must be numeric.
+#' @param data A data frame with columns for Team, Score, Year, and Week produced by scrape_games.  Last 3 columns must be numeric.
 #'
 #' @return A list with 6 elements
 #' @export
@@ -10,11 +10,11 @@
 #' G <- regssn2021
 #' List <- XY_differences(G)
 #' List$teams
-XY_differences <- function(games){
-  N <- length(games$Score)
-  M <- length(unique(games$Team))
+XY_differences <- function(data){
+  N <- length(data$Score)
+  M <- length(unique(data$Team))
   X <- matrix(data = 0, nrow = N/2, ncol = M+1)
-  teams <- sort(unique(games$Team))
+  teams <- sort(unique(data$Team))
   X <- matrix(data = 0, nrow = N/2, ncol = M+1)
   colnames(X) <- c("home", teams)
   Y <- rep(0, N/2)
@@ -22,14 +22,14 @@ XY_differences <- function(games){
   row = 1
   for(n in seq(2,N, by = 2)){
     X[row,"home"] <- 1
-    X[row, games$Team[n-1]] <- -1
-    X[row, games$Team[n]] <- 1
-    Y[row] <- games$Score[n] - games$Score[n-1]
+    X[row, data$Team[n-1]] <- -1
+    X[row, data$Team[n]] <- 1
+    Y[row] <- data$Score[n] - data$Score[n-1]
     row <- row +1
   }
-  start <- paste(games$Year[1], ", week ", games$Week[1])
-  end <- paste(games$Year[N], ", week ", games$Week[N])
-  result <- list(X,Y, teams, games, start, end)
+  start <- paste(data$Year[1], ", week ", data$Week[1])
+  end <- paste(data$Year[N], ", week ", data$Week[N])
+  result <- list(X,Y, teams, data, start, end)
   names(result) <- c("X", "Y_diff", "teams", "games", "start", "end")
   return(result)
 }
---
+++ R/get_design.R
@@ -0,0 +1,42 @@
+
+#' Get design matrix and values from a season
+#'
+#' @param data A data frame with columns for Team, Score, Year, and Week
+#' produced by scrape_games.  Last 3 columns must be numeric.
+#'
+#' @return A list of 9 components, including 2 design matrices (X, X_sum),
+#' and 3 vectors of observations of season data (Y_diff, Y_sum, Y_binary), the
+#' list of teams, original dataframe of scores, and date range of games.
+#' @export
+#'
+#' @examples
+#' games <- regssn2021
+#' L <- get_design(games)
+get_design <- function(data){
+  N <- length(data$Score)
+  M <- length(unique(data$Team))
+  X <- matrix(data = 0, nrow = N/2, ncol = M+1)
+  teams <- sort(unique(data$Team))
+  X <- matrix(data = 0, nrow = N/2, ncol = M+1)
+  colnames(X) <- c("home", teams)
+  Y_diff <- rep(0, N/2)
+  Y_sum <- rep(0, N/2)
+  #Fill in cols of X with 1's where appropriate
+  row = 1
+  for(n in seq(2,N, by = 2)){
+    X[row,"home"] <- 1
+    X[row, data$Team[n-1]] <- -1
+    X[row, data$Team[n]] <- 1
+    Y_diff[row] <- data$Score[n] - data$Score[n-1]
+    Y_sum[row] <- data$Score[n] + data$Score[n-1]
+    row <- row +1
+  }
+  Y_binary <- ifelse(Y_diff >= 0, 1, 0)
+  X_sum <- abs(X[,-1])
+  #start <- paste(data$Year[1], ", week ", data$Week[1])
+  #end <- paste(data$Year[N], ", week ", data$Week[N])
+  result <- list(X,X_sum, Y_diff, Y_sum, Y_binary, teams, data)
+  names(result) <- c("X", "X_sum", "Y_diff", "Y_sum", "Y_binary", "teams",
+                     "games")
+  return(result)
+}
