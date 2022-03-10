6e16f3df6d2e7b1118898105bee94a7becf10621
Lee Przybylski <przybylskily@gmail.com>
2022-03-09 22:06:04 -0500

    Added winprob_emp function with documentation

--- R/winprob.R
+++ R/winprob.R
@@ -1,6 +1,6 @@
 #' Calculate the win probability using OLS estimators
 #'
-#' @param data List of season data as produced by XY_differences, contains
+#' @param data List of season data as produced by get_design(), contains
 #' design matrix X, scores Y_diff, and a list of teams
 #' @param home Name of the home team, could be a substring
 #' @param away Name of the away team, could be a substring
@@ -15,7 +15,7 @@
 #'
 #' @examples
 #' G <- regssn2021
-#' List <- XY_differences(G)
+#' List <- get_design(G)
 #' winprob_ols(List, "Patriots", "Bills")
 winprob_ols <- function(data, home, away, home_effect = TRUE, verbose = TRUE){
   X <- data$X
@@ -47,10 +47,27 @@ winprob_ols <- function(data, home, away, home_effect = TRUE, verbose = TRUE){
 
 
 
+#' Title
+#'
+#' @param design List of season data as produced by get_design(), contains
+#' design matrix X, scores Y_diff, and a list of teams
+#' @param home Name of the home team, could be a substring
+#' @param away Name of the away team, could be a substring
+#' @param home_effect Logical, indicates if there is home field advantage
+#' @param verbose Logical, indicates if we want output to read win probabilities
+#'
+#' @return A data frame containing the teams and their win probabilities
+#'
+#' @export
+#'
+#' @examples
+#' G <- regssn2021
+#' design <- get_design(G)
+#' winprob_emp(design, "Patriots", "Bills")
 winprob_emp <- function(design, home, away, home_effect = TRUE, verbose = TRUE){
   X <- design$X
   Y <- design$Y_dif
-  teams <- data$teams
+  teams <- design$teams
   #Create contrast vector
   home_indx <- which(grepl(home, teams, ignore.case = TRUE))[1]
   away_indx <- which(grepl(away, teams, ignore.case = TRUE))[1]
@@ -63,13 +80,19 @@ winprob_emp <- function(design, home, away, home_effect = TRUE, verbose = TRUE){
   cont <- as.matrix(cont)
   X.X <- t(X)%*%X
   df <- nrow(X) - stats::anova(reg)[1,1]
-  mu <- t(cont)%*%MASS::ginv(X.X)%*%t(X)%*%Y
-  sigsq <- anova(reg)[2,3]
-  sigma <- sqrt(sigsq)
-  AwayProb <- pnorm(0,mean = mu, sd = sigma)
-  HomeProb <- pnorm(0,mean = mu, sd = sigma, lower.tail = FALSE)
-  print(paste("The estimated win probability for the ", AwayTm, "at",
-              HomeTm, "is", round(AwayProb,3), sep = " " ))
+  mu <- (t(cont)%*%MASS::ginv(X.X)%*%t(X)%*%Y)[1]
+  # Use shifted residuals to get win prob
+  res <- residuals(reg)
+  res_shift <- res + mu
+  pos_res_shift <- sum(res_shift > 0)
+  neg_res_shift <- sum(res_shift < 0)
+  AwayProb <- neg_res_shift/length(res)
+  HomeProb <- pos_res_shift/length(res)
+if (verbose){
+  "The estimated win probability for the {AwayTm} at {HomeTm} is {round(AwayProb, 3)}" %>%
+    glue::glue() %>%
+    print()
+}
   probs <- data.frame(h = HomeTm, a = AwayTm, h_spread = mu,
                       h_prob = HomeProb, a_prob = AwayProb, method = "normal")
   return(probs)
