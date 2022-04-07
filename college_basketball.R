95d1d4a37b37cbef57d639651a7aecf377dffaeb
Lee Przybylski <przybylskily@gmail.com>
2022-04-06 20:25:10 -0400

    Predictions for Duke and Arkansas

--- college_basketball.R
+++ college_basketball.R
@@ -2,6 +2,7 @@ data <- read.csv("scores_3-18.csv")
 head(data)
 
 #Load NFLpredictions
+library(devtools)
 load_all()
 library(dplyr)
 
@@ -13,5 +14,9 @@ design <- get_design(data)
 
 #Teams "NotreDame", "TexasTech"
 
-eML_normal(design, "MichiganSt", "Duke", hBL = 215, aBL = -265, home_effect = FALSE)
-espread_normal(design, "MichiganSt", "Duke", hspread = 6.5, home_effect = FALSE)
+eML_normal(design, "Duke", "Arkansas", hBL = -210, aBL = 175, home_effect = FALSE)
+espread_normal(design, 140, "Miami", hspread = -6, home_effect = FALSE)
+espread_emp(design, "IowaSt", "Miami", hspread = 3, home_effect = FALSE)
+winprob_emp(design, "IowaSt", "Miami")
+
+team_detect(design, "Kansas")
