23ee882b5a91ef0024b86f5ff35b57f0dfcf0379
Lee Przybylski <Lee.Przybylski@nationals.com>
2026-02-03 22:59:53 -0500

    prototype daily runner is working

--- Research/daily_runner.R
+++ Research/daily_runner.R
@@ -2,6 +2,12 @@ library(nflreadr)
 pkgload::load_all()
 
 # Quick prediction for the next week's worth of games
+date <- Sys.Date() # Default to current
+
+season <- lubridate::year(date)
+if(lubridate::month(date) < 9) {
+  season <- season - 1
+}
 
 futile.logger::flog.info("Running season %i", season)
 futile.logger::flog.info("Getting schedule from nflreadr")
@@ -28,4 +34,81 @@ games_upcoming <- games %>%
 
 design <- games_wide_to_design(games_past)
 
+design_rank <- design$X %>%
+  rbind(c(0, rep(1, ncol(design$X) - 1))) %>%
+  pracma::Rank()
+
+if(design_rank < ncol(design$X)) {
+  "Design matrix is not full rank, no predictions for Week {wk}" %>%
+    glue::glue() %>%
+    futile.logger::flog.warn()
+
+  #return(NULL)
+}
+
+home <- games_upcoming$home_team
+
+away <- games_upcoming$away_team
+
+date <- games_upcoming$gameday
+
+# add a catch for when the models fail to converge
+
+futile.logger::flog.info("Running models and making predictions")
+
+# OLS Normal
+ols_normal <- winprob_normal(design, home, away)
+ols_normal_preds <- ols_normal$probs %>%
+  select(home, away, p_normal1 = win_prob, sp_ols1 = est_spread) %>%
+  mutate(date = date)
+
+# OLS Empirical
+ols_emp <- winprob_emp(design, home, away)
+ols_emp_preds <- ols_emp$probs %>%
+  select(home, away, p_emp = win_prob) %>%
+  mutate(date = date)
+
+# OLS Normal without home field advantage
+ols_normal2 <- winprob_normal(
+  design, home, away, home_advantage_fit = FALSE
+)
+ols_normal2_preds <- ols_normal2$probs %>%
+  select(home, away, p_normal2 = win_prob, sp_ols2 = est_spread) %>%
+  mutate(date = date)
+
+# Logistic
+logistic <- winprob_logistic(design, home, away)
+logistic_preds <- logistic$probs %>%
+  select(home, away, p_logistic = win_prob) %>%
+  mutate(date = date)
+
+# Logistic without home field advantage
+logistic2 <- winprob_logistic(design, home, away, home_advantage_fit = FALSE)
+
+logistic2_preds <- logistic2$probs %>%
+  select(home, away, p_logistic2 = win_prob) %>%
+  mutate(date = date)
+
+# Combine predictions by leftjoin to weeks games using home, away, and week
+output <- games_upcoming %>%
+  left_join(
+    ols_normal_preds,
+    by = c("home_team" = "home", "away_team" = "away", "gameday" = "date")
+  ) %>%
+  left_join(
+    ols_emp_preds,
+    by = c("home_team" = "home", "away_team" = "away", "gameday" = "date")
+  ) %>%
+  left_join(
+    ols_normal2_preds,
+    by = c("home_team" = "home", "away_team" = "away", "gameday" = "date")
+  ) %>%
+  left_join(
+    logistic_preds,
+    by = c("home_team" = "home", "away_team" = "away", "gameday" = "date")
+  ) %>%
+  left_join(
+    logistic2_preds,
+    by = c("home_team" = "home", "away_team" = "away", "gameday" = "date")
+  )
 
